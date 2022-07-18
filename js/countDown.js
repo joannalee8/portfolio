8c1a7c0702dea36c8309ba8121ab8720ab7eac84
Joanna <jongeun0114@gmail.com>
2022-07-18 15:33:24 -0700

    update

---
+++ js/countDown.js
@@ -0,0 +1,26 @@
+var countDownDate = new Date("April 13, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("countdown").innerHTML = days + ":" + hours + ":"
+  + minutes + ":" + seconds;
+    
+  // If the count down is over, this shows up 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "00:00:00:00";
+  }
+}, 1000);
